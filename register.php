d60794df22b41738cd6b0178aebae77c8999593e
jooprima <jooprima@gmail.com>
2019-11-20 19:12:57 +0700

    proses register

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <div id="container-user-akses">
     
-    <form action="<?php echo BASE_URL."proses_register"; ?>" method="POST">
+    <form action="<?php echo BASE_URL."proses_register.php"; ?>" method="POST">
 
         <div class="element-form">
             <label for="">Nama Lengkap</label>
