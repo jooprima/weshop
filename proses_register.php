5a30a8dd65562c12348da6125cc27ce093429ada
jooprima <jooprima@gmail.com>
2019-11-20 20:00:25 +0700

    pengecekan require form

--- proses_register.php
+++ proses_register.php
@@ -11,8 +11,19 @@
     $alamat = $_POST['alamat'];
     $password = md5($_POST['password']);
     $re_password = $_POST['re_password'];
+    
+    unset($_POST['password']);
+    unset($_POST['re_password']);
 
-    mysqli_query($koneksi, "insert into user (level,nama,email,alamat,phone,password,status)
+    $dataForm = http_build_query($_POST);
+
+    if(empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password)) {
+        header("location: ".BASE_URL."index.php?page=register&notif=require&$dataForm");
+    }else{
+        mysqli_query($koneksi, "insert into user (level,nama,email,alamat,phone,password,status)
                                 values ('$level','$nama_lengkap','$email','$alamat','$phone','$password','$status')");
+    }
+
+    
 
 ?>
