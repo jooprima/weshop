d027d60642bdf47d17e40c17a5a2290f37a3658d
jooprima <jooprima@gmail.com>
2019-11-21 10:37:16 +0700

    halaman login & logout

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
+    session_start();
+
     include_once("function/helper.php");
 
     $page = isset($_GET['page']) ? $_GET['page'] : false;
 
+    $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
+    $nama = isset($_SESSION['nama']) ? $_SESSION['nama'] : false;
+    $level = isset($_SESSION['level']) ? $_SESSION['level'] : false;
 
 ?>
 
@@ -26,8 +31,17 @@
 
             <div id="menu">
                 <div id="user">
-                    <a href="<?php echo BASE_URL."index.php?page=login"; ?>">Login</a>
-                    <a href="<?php echo BASE_URL."index.php?page=register"; ?>">Register</a>
+                    <?php 
+                        if($user_id){
+                            echo "Hai <b>$nama</b>,  
+                            <a href='".BASE_URL."index.php?page=my_profile&module=pesanan&action=list'>My Profile  </a>
+                            <a href='".BASE_URL."logout.php'>Logout</a>";
+                        }else{
+                            echo "<a href='".BASE_URL."index.php?page=login'>Login</a>
+                                    <a href='".BASE_URL."index.php?page=register'>Register</a>";
+                        }
+                    ?>
+                    
                 </div>
                 <a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>" id="button-keranjang">
                     <img src="<?php echo BASE_URL."images/cart.png"; ?>" />
