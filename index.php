c4e9ae451b743d8f86dfc2210dae0a57f506d5d9
jooprima <jooprima@gmail.com>
2019-11-23 02:26:39 +0700

    add halaman keranjang

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : false;
     $nama = isset($_SESSION['nama']) ? $_SESSION['nama'] : false;
     $level = isset($_SESSION['level']) ? $_SESSION['level'] : false;
+    $keranjang = isset($_SESSION['keranjang']) ? $_SESSION['keranjang'] : false;
+    $totalBarang = count($keranjang);
 
 ?>
 
@@ -63,6 +65,11 @@
                 </div>
                 <a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>" id="button-keranjang">
                     <img src="<?php echo BASE_URL."images/cart.png"; ?>" />
+                    <?php
+                        if ($totalBarang != 0) {
+                            echo "<span class='total-barang'>$totalBarang</span>";
+                        }
+                    ?>
                 </a>
             </div>
 
